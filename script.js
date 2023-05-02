660b31c076e953c20ae12b23fc6e966ba7128dc6
wessam-m-elsheikh <96388872+wessam-m-elsheikh@users.noreply.github.com>
2023-05-02 17:19:58 +0300

    welcome section

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// ///////////////////// SELECTIONS ////////////////////
+const menuOpen = document.querySelector(".drop-down-open");
+const menuClose = document.querySelector(".drop-down-close");
+const menu = document.querySelector(".nav-list");
+
+// ///////////////////// FUNCTIONS ////////////////////
+// open menu
+function openMenu() {
+  menu.style.display = "flex";
+  menuOpen.style.display = "none";
+  menuClose.style.display = "inline-block";
+  // navItems.forEach((item) => {
+  //   item.style.animation = "drop-menu 0.3s linear forwards";
+  //   item.style.opacity = "1";
+  // });
+}
+
+// close menu
+function closeMenu() {
+  menu.style.display = "none";
+  menuClose.style.display = "none";
+  menuOpen.style.display = "inline-block";
+}
+
+////////////////////////////////////////
+// show and hide menu
+menuOpen.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
+// to make sure both icon and menu appear on proper screen sizes
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 900) {
+    menuOpen.style.display = "none";
+    menuClose.style.display = "none";
+    menu.style.display = "flex";
+  } else {
+    menuOpen.style.display = "inline-block";
+    menu.style.display = "none";
+  }
+});
+
+window.addEventListener("load", () => {
+  if (window.innerWidth <= 900) {
+    menuOpen.style.display = "inline-block";
+    menu.style.display = "none";
+    menuClose.style.display = "none";
+  }
+});
