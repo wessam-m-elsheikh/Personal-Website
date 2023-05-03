c93d6c50b5bbee4895f8b0fa43085833a45218cb
wessam-m-elsheikh <96388872+wessam-m-elsheikh@users.noreply.github.com>
2023-05-03 20:53:12 +0300

    carousel
    projects section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// ///////////////////// SELECTIONS ////////////////////
+// ///////////////////// DROP MENU ////////////////////
 const menuOpen = document.querySelector(".drop-down-open");
 const menuClose = document.querySelector(".drop-down-close");
 const menu = document.querySelector(".nav-list");
@@ -47,3 +47,39 @@ window.addEventListener("load", () => {
     menuClose.style.display = "none";
   }
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ///////////////////// SLIDER ///////////////////////
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".slider-btn-right");
+const previous = document.querySelector(".slider-btn-left");
+const dotsContainer = document.querySelector(".dots");
+
+let currentSlide = 0;
+const maxSlides = slides.length - 1;
+
+// ///////////////////// FUNCTIONS ////////////////////
+// create dots
+const createDots = function () {
+  slides.forEach((_, i) => {
+    dotsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+// show active dot
+const activeDot = function (slideNumber) {
+  // deactivating them all before passing the active class
+  document
+    .querySelectorAll(".dot")
+    .forEach((dot) => dot.classList.remove("dot-active"));
+
+  // then activating the currentslide's dot
+  // deactivating them all before passing the active class
+  document
+    .querySelector(`.dot[data-slide=${slideNumber}]`)
+    .classList.add("dot-active");
+};
